cde999654098a0990ab79630e31ff8e598893e32
Dheerendra Yadav <dheerendray@smartdatainc.net>
2023-04-16 12:08:08 +0530

    db setting changes

--- healthcare/settings.py
+++ healthcare/settings.py
@@ -94,24 +94,24 @@ WSGI_APPLICATION = 'healthcare.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # #Production database
-# DATABASES = {
-#   'default': {
-#     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#     'NAME': "medical-app-db",
-#     'USER': "medical_app_db",
-#     'PASSWORD': "Medicalapp2023",
-#     'HOST': "medical-app-db.capltgczwktt.eu-north-1.rds.amazonaws.com",
-#     'PORT': "5432",
-#   }
-# }
+DATABASES = {
+  'default': {
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': "medical-app-db",
+    'USER': "medical_app_db",
+    'PASSWORD': "Medicalapp2023",
+    'HOST': "medical-app-db.capltgczwktt.eu-north-1.rds.amazonaws.com",
+    'PORT': "5432",
+  }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
